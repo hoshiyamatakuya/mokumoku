b08799a1c1e0865f910051366f33fcd1dfb23507
hoshiyamatakuya <andytakuya4166@gmail.com>
2022-10-21 17:11:14 +0900

    テストもok

--- config/application.rb
+++ config/application.rb
@@ -41,8 +41,9 @@ module Mokumoku
     end
 
     config.i18n.default_locale = :ja
-    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.paths.add 'lib', eager_load: true
   end
 end
